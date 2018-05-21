1dc8f36889353c686e666c7c22b75ff6ecf180ca
pep <blizz.pep@gmail.com>
2018-05-20 21:03:10 -0300

    ajustando estrutura

--- Infra/TokenConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace aspNetCore.Infra
-{
-	public class TokenConfigurations
-	{
-		public string Audience { get; set; }
-		public string Issuer { get; set; }
-		public int Seconds { get; set; }
-	}
-}
